b163ccd17ff18c92b8485f8916837d85470c7923
Javier Vegas <javiervegas@yahoo.com>
2012-11-17 02:04:22 -0800

    get articles from twitter api

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -1,12 +1,30 @@
 package models
+import scala.collection.JavaConversions._
+import twitter4j.TwitterFactory
+import twitter4j.Twitter
+import twitter4j.conf.ConfigurationBuilder
 
 case class Article(id:Long, title:String, content:String)
 
 object Article {
   
-  var articles = (1L to 4L).map { i => Article(i, "tit", "cont1") }
+  val cb = new ConfigurationBuilder()
+  cb.setDebugEnabled(true)
+    .setOAuthConsumerKey("1mQSCHfh4duLVSzmLsBQ")
+    .setOAuthConsumerSecret("xwkn0bjerCIzY1hmcXlUnpSl6tdTaJkFADanJo1Kwh8")
+    .setOAuthAccessToken("14998341-m8nDySA6O8ByHin9JyVJA5cXMuD1JlTJt2XpFJhlI")
+    .setOAuthAccessTokenSecret("LhYQQ0IihaajHlRLlV5xqtMjLLUlUXPR4dwMRIA")
+  val tf = new TwitterFactory(cb.build())
+  val twitter = tf.getInstance()
 
-  def findAll = this.articles.toList.sortBy(_.id) 
-
+  def findAll = { 
+    var articles = List[Article]()
+    val it = twitter.getHomeTimeline.iterator
+    while (it.hasNext) {
+      val status = it.next
+      articles  = Article(1, status.getUser.getName, status.getText) :: articles
+    }
+    articles
+  }
 }
 
