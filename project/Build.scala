c6a6f80990e40059e3a79570ad883e0b00636338
Javier Vegas <javiervegas@yahoo.com>
2012-11-17 02:04:22 -0800

    get articles from twitter api

--- project/Build.scala
+++ project/Build.scala
@@ -9,6 +9,7 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "org.twitter4j" % "twitter4j-core" % "[2.2,)"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
