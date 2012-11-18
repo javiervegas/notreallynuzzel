0369e9e6728bdad01d63abd219078e1676c83ff1
Javier Vegas <javiervegas@yahoo.com>
2012-11-18 11:01:59 -0800

    simplify, show profile photos

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -1,11 +1,8 @@
 package models
 import scala.collection.JavaConversions._
-import twitter4j.Paging
-import twitter4j.Twitter
-import twitter4j.TwitterFactory
+import twitter4j._
 
-case class Article(url:String, tweets:List[Tweet])
-case class Tweet(author:String, content:String)
+case class Article(url:String, tweets:List[Status])
 
 object Article {
   
@@ -13,8 +10,8 @@ object Article {
 
   def findAll = { 
     val tweets = twitter.getHomeTimeline(new Paging(1,100)).iterator.toList.filterNot { _.getURLEntities.isEmpty }
-    tweets.foldLeft(Map[String, List[Tweet]]() withDefaultValue List[Tweet]()){
-      (m,s) => m + (s.getURLEntities.head.getDisplayURL -> (m(s.getURLEntities.head.getDisplayURL) ++ List(Tweet(s.getUser.getName, s.getText)) ) )
+    tweets.foldLeft(Map[String, List[Status]]() withDefaultValue List[Status]()){
+      (m,s) => m + (s.getURLEntities.head.getDisplayURL -> (m(s.getURLEntities.head.getDisplayURL) ++ List(s)) )
     }.map{ case (k,v) => Article(k,v) }.toList sortBy { a => (-a.tweets.size, a.url) }
   }
 }
