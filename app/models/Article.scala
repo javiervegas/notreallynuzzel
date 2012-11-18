46a6ed9ce39c3c67b4b5f3708fa020ee0ad8f5ae
Javier Vegas <javiervegas@yahoo.com>
2012-11-18 01:04:57 -0800

    refactor

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -4,14 +4,18 @@ import twitter4j.Paging
 import twitter4j.Twitter
 import twitter4j.TwitterFactory
 
-case class Article(url:String, author:String, content:String)
+case class Article(url:String, tweets:List[Tweet])
+case class Tweet(author:String, content:String)
 
 object Article {
   
   val twitter = (new TwitterFactory).getInstance
 
   def findAll = { 
-    twitter.getHomeTimeline(new Paging(1,100)).iterator.toList.filterNot {_.getURLEntities.isEmpty }.map { s => Article(s.getURLEntities.head.getDisplayURL, s.getUser.getName, s.getText) } 
+    val tweets = twitter.getHomeTimeline(new Paging(1,100)).iterator.toList.filterNot { _.getURLEntities.isEmpty }
+    tweets.foldLeft(Map[String, List[Tweet]]() withDefaultValue List[Tweet]()){
+      (m,s) => m + (s.getURLEntities.head.getDisplayURL -> (m(s.getURLEntities.head.getDisplayURL) ++ List(Tweet(s.getUser.getName, s.getText)) ) )
+    }.map{ case (k,v) => Article(k,v) }.toList
   }
 }
 
