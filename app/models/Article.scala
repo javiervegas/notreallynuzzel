72d22a6656c84e22e3e03455a1a3150fd1bdb295
Javier Vegas <javiervegas@yahoo.com>
2012-11-17 23:28:53 -0800

    show tweets with links

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -1,16 +1,17 @@
 package models
 import scala.collection.JavaConversions._
-import twitter4j.TwitterFactory
+import twitter4j.Paging
 import twitter4j.Twitter
+import twitter4j.TwitterFactory
 
-case class Article(id:Long, title:String, content:String)
+case class Article(url:String, author:String, content:String)
 
 object Article {
   
   val twitter = (new TwitterFactory).getInstance
 
   def findAll = { 
-    twitter.getHomeTimeline.iterator.toList.map { s => Article(s.getId, s.getUser.getName, s.getText) } 
+    twitter.getHomeTimeline(new Paging(1,100)).iterator.toList.filterNot {_.getURLEntities.isEmpty }.map { s => Article(s.getURLEntities.head.getDisplayURL, s.getUser.getName, s.getText) } 
   }
 }
 
