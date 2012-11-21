a4c1557bd9bfc33a0fa41b71ad0e094a58a1ae74
Javier Vegas <javiervegas@yahoo.com>
2012-11-21 11:39:09 -0800

    callback debug

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,7 +13,9 @@ object Application extends Controller {
     Cache.get("request_token").asInstanceOf[Option[RequestToken]] match {
       case Some(rt:RequestToken) => Ok(views.html.index()) 
       case None => {
-        val request_token = (new TwitterFactory).getInstance.getOAuthRequestToken("http://"+request.host+routes.Application.callback())
+        val callback_url = "http://"+request.host+routes.Application.callback()
+        println("setting callback_url to "+callback_url)
+        val request_token = (new TwitterFactory).getInstance.getOAuthRequestToken(callback_url)
         Cache.set("request_token",request_token, 60)
         Redirect(request_token.getAuthenticationURL)
       }
@@ -21,7 +23,9 @@ object Application extends Controller {
   }
   
   def callback = Action { request =>
-    Cache.set("oauth_verifier", request.queryString.get("oauth_verifier").get.mkString) 
+    val oauth_verifier = request.queryString.get("oauth_verifier").get.mkString
+    println("in callback, setting oauth_verifier to "+oauth_verifier)
+    Cache.set("oauth_verifier", request.queryString.get("oauth_verifier").get.mkString, 60) 
     Redirect(routes.Application.index())
   }
   
