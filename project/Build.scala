b68624df842ed1f7b7d78b0f0b466bb7d63a4b0e
Javier Vegas <javiervegas@yahoo.com>
2012-11-23 20:18:51 -0800

    upgrade to twitter4j 3.0.1

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       // Add your project dependencies here,
       //"org.twitter4j" % "twitter4j-core" % "[2.2,)",
-      "org.twitter4j" % "twitter4j-core" % "2.2.6",
+      "org.twitter4j" % "twitter4j-core" % "3.0.1",
       "org.scalaj" %% "scalaj-http" % "0.3.2",
       "org.jsoup" % "jsoup" % "0.2.1b"
     )
