c27e539f18a74e9e048339c3d77084e3e87616fa
Javier Vegas <javiervegas@yahoo.com>
2012-11-26 17:19:17 -0800

    increase cache TTL, test user

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,7 @@ import play.api.mvc._
 import play.api.Play.current
 import twitter4j._
 import twitter4j.auth.RequestToken
+import twitter4j.conf.ConfigurationBuilder
 
 object Application extends Controller {
   
@@ -23,7 +24,7 @@ object Application extends Controller {
       case Some(twitter:Twitter) => Ok(views.html.index(name match {
         case Some(name:String) => "@"+name+"'s"
         case None => "your"
-      }, twitter.verifyCredentials.getScreenName, Cache.getAs[Map[String,Twitter]]("users").get.keySet)).withSession( 
+      }, twitter.verifyCredentials.getScreenName, Cache.getAs[Map[String,Twitter]]("users").get.keySet-"test")).withSession( 
         request.session + ("name" -> (name match {
           case Some(name:String) => name
           case None => twitter.verifyCredentials.getScreenName
@@ -33,7 +34,7 @@ object Application extends Controller {
         val callback_url = "http://"+request.host+routes.Application.callback()
         println("setting callback_url to "+callback_url)
         val request_token = (new TwitterFactory).getInstance.getOAuthRequestToken(callback_url)
-        Cache.set(uuid+"_request_token",request_token, 600)
+        Cache.set(uuid+"_request_token",request_token, 60*60*24)
         Redirect(request_token.getAuthenticationURL).withSession( 
           request.session + ("uuid" -> uuid )
         ) 
@@ -51,7 +52,13 @@ object Application extends Controller {
     Cache.set(uuid+"_twitter", twitter) 
     Cache.set("users", (Cache.getAs[Map[String,Twitter]]("users") match { 
       case Some(m:Map[String,Twitter]) => m
-      case None => Map[String,Twitter]()
+      case None => {
+        val cb = new ConfigurationBuilder()
+          .setOAuthAccessToken("6204-EnlKWhDit3RT3RCvvnDqBsO62VeRju7gBbiHhenvo")
+          .setOAuthAccessTokenSecret("LfGl7RpmiwmjBwOw3yuPe94FA5yTHLnvqgUEpgrUQY")
+        val tf = new TwitterFactory(cb.build())
+        Map[String,Twitter]("test" -> tf.getInstance() )
+      }
     }) + (twitter.verifyCredentials.getScreenName -> twitter ) )
     Redirect(routes.Application.index())
   }
